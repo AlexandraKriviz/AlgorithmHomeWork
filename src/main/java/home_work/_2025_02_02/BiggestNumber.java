acefdc94640639adedc4a79f78bf2b3d85133220
Alexandra_Kriviz <kriviz.alexandra@gmail.com>
2025-02-09 18:07:50 +0100

    Home Work for 02.02.2025.

--- src/main/java/home_work/_2025_02_02/BiggestNumber.java
+++ src/main/java/home_work/_2025_02_02/BiggestNumber.java
@@ -7,13 +7,11 @@ public class BiggestNumber {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
-            // Ввод трех чисел
             System.out.print("Введите три числа: ");
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             int c = scanner.nextInt();
 
-            // Находим наибольшее число
             if (b > a) {
                 a = b;
             }
@@ -21,7 +19,6 @@ public class BiggestNumber {
                 a = c;
             }
 
-            // Вывод результата
             System.out.println("Наибольшее число: " + a);
         }
     }
